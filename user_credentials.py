b9bf17beae9720958af8fa6830f054067662f9fd
KimeuLoise <kimeuloise@gmail.com>
2020-10-16 15:50:35 +0300

    codes creation and insertion

--- user_credentials.py
+++ user_credentials.py
@@ -1,14 +1,13 @@
 import pyperclip
 import random
 import string
-# Global Variables
+
 global users_list 
 class User:
 	'''
 	Class to create user accounts and save their information
 	'''
-	# Class Variables
-	# global users_list
+
 	users_list = []
 	def __init__(self,first_name,last_name,password):
 		'''
